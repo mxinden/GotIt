64f4dce273c21bfc9c46f94d97009bd56839e5c4
minden <IndenML@gmail.com>
2015-08-27 17:43:32 +0200

    Create-lecture-button implementation

--- client/templates/application/landing_page.js
+++ client/templates/application/landing_page.js
@@ -6,6 +6,19 @@ Template.landingPage.events({
     lectureID = $(e.target).find('#lectureID').val();
 
     Router.go('lecturePage', {_id: lectureID});
+  },
+
+  /* Create new lecture */
+  'click #createClassroom': function (event) {
+    event.preventDefault();
+
+    Meteor.call('lectureInsert', function(error, result){
+      //Display error
+      if(error)
+        return alert(error.reason);
+      
+      Router.go('lecturePage', {_id: result._id});
+    });
   }
 
 });
