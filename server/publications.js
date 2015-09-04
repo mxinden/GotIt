bc3cca39c08534fb769a4c721ed21d48ac48d605
minden <IndenML@gmail.com>
2015-09-04 19:22:58 +0200

    Merge develop into feature/add-testing-framework

--- server/publications.js
+++ server/publications.js
@@ -1,16 +1,16 @@
-Meteor.publish('lectures', function() {
-  return Lectures.find();
+Meteor.publish('lecture', function(lectureCode) {
+  return Lectures.find({lectureCode: lectureCode});
 });
 
 Meteor.publish('questions', function(lectureCode) {
-  return Questions.find({'lectureCode': lectureCode});
+  return Questions.find({lectureCode: lectureCode});
 });
 
 Meteor.publish('votes', function(lectureCode) {
-  return Votes.find({'lectureCode': lectureCode});
+  return Votes.find({lectureCode: lectureCode});
 });
 
 /** Publish which user is present in the current lecture */
 Meteor.publish('presences', function(lectureCode) {
-  return Presences.find({"state.currentLectureCode": lectureCode});
+  return Presences.find({'state.currentLectureCode': lectureCode});
 });
