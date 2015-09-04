c9a1fb53c6f075cac92566cb9478d352f69bfa2f
minden <IndenML@gmail.com>
2015-09-05 00:16:15 +0200

    Add fixtures (debugOnly) package for testing environment and load script in /test folder

---
+++ packages/fixtures/fixtures.js
@@ -0,0 +1,45 @@
+if (process.env.IS_MIRROR) {
+  Meteor.methods({
+    'loadFixtures': function(){
+      console.log('Loading default fixtures');
+
+      Lectures.insert({
+        lectureCode: '00000',
+        title: 'OpenSource150904'
+      });
+
+      console.log('Finished loading default fixtures');
+    },
+
+    'clearDB': function(){
+      console.log('Clear DB');
+
+      var collectionsRemoved = 0;
+      var db = Meteor.users.find()._mongo.db;
+      db.collections(function (err, collections) {
+
+        var appCollections = _.reject(collections, function (col) {
+          return col.collectionName.indexOf('velocity') === 0 ||
+            col.collectionName === 'system.indexes';
+        });
+
+        _.each(appCollections, function (appCollection) {
+          appCollection.remove(function (e) {
+            if (e) {
+              console.error('Failed removing collection', e);
+              fut.return('fail: ' + e);
+            }
+            collectionsRemoved++;
+            console.log('Removed collection');
+            if (appCollections.length === collectionsRemoved) {
+              console.log('Finished resetting database');
+            }
+          });
+        });
+
+      });
+
+      console.log('Finished clearing');
+    }
+  });
+}
