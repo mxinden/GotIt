4a2da4e6c9e56b2aa6743deb2e047c77b9685720
minden <IndenML@gmail.com>
2015-09-05 03:55:15 +0200

    Fix bug for 'Create Classroom' button test and add test for 'Enter Classroom' button

--- tests/jasmine/client/integration/landingPageSpec.js
+++ tests/jasmine/client/integration/landingPageSpec.js
@@ -2,12 +2,17 @@ describe("landingPage", function(){
 
   describe("template", function(){
 
-    beforeAll(function(done) {
+    beforeEach(function(done) {
       Router.go('landing_page');
       Tracker.afterFlush(done);
     });
 
     beforeEach(waitForRouter);
+
+
+    it("landing_page", function(){
+      expect(Router.current().route.getName()).toEqual('landing_page');
+    });
 
     it("shows the 'Classroom Code' input field", function() {
       expect($('input#lectureID')).toExist();
@@ -21,19 +26,37 @@ describe("landingPage", function(){
       expect($('button#createClassroom')).toExist();
     });
 
-    describe("'Create Classroom' button on the landing page", function() {
+    describe("'Create Classroom' button", function(done) {
 
       var amountLecturesBefore = Lectures.find().count();
 
-      it("routes to a new lecture", function(done){
-        $('#createClassroom').trigger("click");
-        done();
-        expect(Router._currentRoute.getName()).toEqual('lecturePage');
+      it("routes to a lecture", function(done){
+        $('#createClassroom').click();
+        waitForElement('#questionText', function(){
+          expect(Router.current().route.getName()).toEqual('lecturePage');
+          done();
+        });
+
       });
 
-      it("creates an new lecture document in the lectures collection", function() {
+      it("creates an new lecture document in the lectures collection", function(done) {
         var amountLecturesAfter = Lectures.find().count();
         expect(amountLecturesAfter).toBeGreaterThan(amountLecturesBefore);
+        done();
+      });
+
+    });
+
+    describe("'Enter Classroom' button", function() {
+
+      it("routes to a lecture page", function(done){
+        var lectureCode = '00000';
+        $('#lectureID').val(lectureCode);
+        $('#btn-enter-class').trigger("click");
+        waitForElement('#questionText', function() {
+          expect(Router._currentRoute.getName()).toEqual('lecturePage');
+          done();
+        });
       });
 
     });
