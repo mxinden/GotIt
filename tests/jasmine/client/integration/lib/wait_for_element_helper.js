796099581dc347b25baf961f4a14c509005fcdf4
minden <IndenML@gmail.com>
2015-09-05 03:53:42 +0200

    Add waitForElement function to wait for DOM Element in a test

---
+++ tests/jasmine/client/integration/lib/wait_for_element_helper.js
@@ -0,0 +1,14 @@
+window.waitForElement = function (selector, successCallback) {
+  var checkInterval = 50;
+  var timeoutInterval = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+  var startTime = Date.now();
+  var intervalId = Meteor.setInterval(function () {
+    if (Date.now() > startTime + timeoutInterval) {
+      Meteor.clearInterval(intervalId);
+      // Jasmine will handle the test timeout error
+    } else if ($(selector).length > 0) {
+      Meteor.clearInterval(intervalId);
+      successCallback();
+    }
+  }, checkInterval);
+};
