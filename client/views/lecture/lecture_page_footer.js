1385d7facd986539eccf3b0de2574df3f5224466
Kristian Andersson <kriax@me.com>
2015-09-06 20:06:28 +0200

    Added call to voteInsert when creating a new question

--- client/views/lecture/lecture_page_footer.js
+++ client/views/lecture/lecture_page_footer.js
@@ -3,17 +3,35 @@ Template.lecturePageFooter.events({
   /** Create new question */
   'submit form': function (event) { 
     event.preventDefault();
-
+    var lectureCode = this.lectureCode
     var question = {
-      'lectureCode': this.lectureCode, 
+      'lectureCode': lectureCode, 
       'questionText': $(event.target).find('#question-text').val()
     };
 
     Meteor.call('questionInsert',question, function(error, result){
-      /** Display error */
-      if(error)
-        return alert(error.reason);
+    
+        if(error){
+            return alert(error.reason);
+        }else{
+            questionId = result; 
+
+            var vote = {
+                questionId: questionId,
+                lectureCode: lectureCode
+            };
+
+            Meteor.call('voteInsert', vote, function(error, result){
+                /** Display error */
+                if(error)
+                    return alert(error.reason);
+            });
+
+        }
+        
     });
+      
+      
 
     event.target.reset();
 
