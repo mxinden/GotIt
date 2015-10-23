f0c1d9f0cc79b5a21eccdc513ae3391fe1d0b55e
minden <IndenML@gmail.com>
2015-10-24 00:54:04 +0200

    Remove prevent default

--- client/views/lecture/lecture_page_header.js
+++ client/views/lecture/lecture_page_header.js
@@ -9,7 +9,6 @@ Template.lecturePageHeader.helpers({
 Template.lecturePageHeader.events({
 
   'click #back-button': function(event) {
-    event.preventDefault();
     Meteor.call('deleteVotesFromUserFromLecture', this.lectureCode, function(error, result) {
       if(error) 
         return alert(error);
