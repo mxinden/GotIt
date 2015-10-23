57c9d3b78f127d822339d2dcc0fbc8116b3fc2e0
minden <IndenML@gmail.com>
2015-10-23 23:29:20 +0200

    Remove votes and go to landing page when leaving room
    When the back-button is clicked all the votes from the user from this lecture are deleted and he is redirected to the landing page

--- client/views/lecture/lecture_page_header.js
+++ client/views/lecture/lecture_page_header.js
@@ -5,3 +5,19 @@ Template.lecturePageHeader.helpers({
   }
 
 }); 
+
+Template.lecturePageHeader.events({
+
+  'click #back-button': function(event) {
+    event.preventDefault();
+    Meteor.call('deleteVotesFromUserFromLecture', this.lectureCode, function(error, result) {
+      if(error) {
+        return alert(error);
+      } else {
+        Router.go('landingPage');
+      }
+    });
+  }
+
+});
+
