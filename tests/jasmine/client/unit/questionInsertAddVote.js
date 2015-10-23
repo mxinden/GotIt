729869cc41fef80aa5f2aa9c9e9a896b4ed1a8ae
minden <IndenML@gmail.com>
2015-10-24 01:55:51 +0200

    Add tests for no submitted questions without proper lecture code

--- tests/jasmine/client/unit/questionInsertAddVote.js
+++ tests/jasmine/client/unit/questionInsertAddVote.js
@@ -64,4 +64,37 @@ describe('questionInsertAddVote', function() {
 
   });
 
+  describe("with a invalid lecture code", function() {
+
+    var callError;
+    var callResult;
+    var amountQuestionsBefore = Questions.find().count();
+    var amountQuestionsAfter;
+
+    var question = {
+      lectureCode: 'notAValidLectureCode', 
+      questionText: 'Is this a valid lecture code?'
+    }
+
+    beforeAll(function(done) {
+      Meteor.call('questionInsertAddVote', question, function(error, result) {
+        callError = error;
+        callResult = result;
+        done();
+      });
+    });
+
+    it("throws an error", function() {
+      expect(callResult).toBe(undefined);
+      expect(callError).not.toBe(undefined);
+      expect(callError.error).toEqual("Not a valid lecture code");
+    });
+
+    it("does not create a new question in the mongodb", function() {
+      amountQuestionsAfter = Questions.find().count();
+      expect(amountQuestionsBefore).toEqual(amountQuestionsAfter);
+    });  
+
+  });
+
 });
