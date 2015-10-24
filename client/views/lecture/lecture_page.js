63343635711aa594fb2e641b7a07bf1f3725a877
minden <IndenML@gmail.com>
2015-10-24 18:35:07 +0200

    Sort questions by amount of votes

--- client/views/lecture/lecture_page.js
+++ client/views/lecture/lecture_page.js
@@ -1,7 +1,19 @@
 Template.lecturePage.helpers({
 
   questions: function() {
-    return Questions.find();
+    var questions =  Questions.find({lectureCode: this.lectureCode}).fetch();
+
+    for (var i = 0; i< questions.length; i++) {
+      var question = questions[i];
+      var amountVotes = Votes.find({questionId: question._id}).count();
+      question.amountVotes = amountVotes;
+    }
+
+    questions.sort(function(a,b) {
+      return b.amountVotes - a.amountVotes;
+    });
+
+    return questions;
   },
 
 });
