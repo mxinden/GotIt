ba39cc2402761798b233b9b0b5d151a37259dc60
minden <IndenML@gmail.com>
2015-11-02 04:54:10 +0100

    Adjust questions_sort_by_amount_votes to new fixtures

--- tests/jasmine/client/integration/features/questions_sort_by_amount_votes.js
+++ tests/jasmine/client/integration/features/questions_sort_by_amount_votes.js
@@ -1,6 +1,28 @@
 describe("Questions on the lecture page", function() {
 
-  var lectureCode = 'sortQuestions';
+  var lectureCode;
+
+  beforeAll(function(done) {
+    Fixtures.clearDB(done);
+  });
+
+  beforeAll(function(done) {
+    Fixtures.createLecture({}, function(error, result) {
+      lectureCode = result;
+      done();
+    });
+  });
+
+  beforeAll(function(done) {
+    Fixtures.createQuestion({questionText: 'First'}, function(error, result) {
+      done();
+    });
+  });
+  beforeAll(function(done) {
+    Fixtures.createQuestion({questionText: 'Second', _id: '11111111111111111'}, function(error, result) {
+      done();
+    });
+  });
 
   beforeAll(function(done) {
     Router.go('lecturePage', {lectureCode: lectureCode});
@@ -10,49 +32,17 @@ describe("Questions on the lecture page", function() {
   beforeAll(waitForRouter);
 
   beforeAll(function(done) {
-    var firstQuestionExists = false;
-    var secondQuestionExists = false;
-    $('#question-text').val('First');
-    $('#create-question').click();
-    var checkExist1 = setInterval(function() {
-      if($('.question:eq(0)').length == 1) {
-        firstQuestionExists = true;
-        clearInterval(checkExist1);
-      }
-    }, 100);
-
-    $('#question-text').val('Second');
-    $('#create-question').click();
-    var checkExist2 = setInterval(function() {
-      if(firstQuestionExists && $('.question:eq(1)').length == 1) {
-        secondQuestionExists = true;
-        clearInterval(checkExist2);
-      }
-    }, 100);
-
-    var waitForSecondQuestion = setInterval(function() {
-      if(secondQuestionExists) {
-        $('.question:eq(0) .btn-unvote').click();
-        clearInterval(waitForSecondQuestion);
-        done();
-      }
-    }, 100);
-  });
-
-
-  it("reorder when votes are changed", function(done) {
+    $('.question:eq(0) .btn-unvote').click();
     var checkExist = setInterval(function() {
       if($('.question:eq(1) .btn-vote').length == 1) {
-        expect(true).toBe(true);
         clearInterval(checkExist);
         done();
       }
     }, 100);
   });
 
+  it("reorder when votes are changed", function() {
+    expect(true).toBe(true);
+  });
+
 });
-
-
-
-
-
