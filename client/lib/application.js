e86ead3012a0ef94ec027508b8c83ddaa5301977
Kristian Andersson <kriax@me.com>
2015-11-03 16:22:02 +0100

    Change getNumberOfMembersInLecture so it doesn't count the lecture creator if the lecture creator is present

--- client/lib/application.js
+++ client/lib/application.js
@@ -1,4 +1,14 @@
 getNumberOfMembersInLecture =  function(lectureCode) {
-  return Lectures.findOne({lectureCode: lectureCode}, {members: true}).members.length;
+
+   lecture = Lectures.findOne({lectureCode: lectureCode}, {members: true, lectureCreator: true});//.members.length;
+   lectureCreator = Lectures.findOne({lectureCode: lectureCode,members: lecture.lectureCreator }, {members: true, lectureCreator: true});
+  
+  if(lectureCreator !== undefined){
+    return lecture.members.length - 1 
+  }
+  else{
+    return lecture.members.length
+  }
+
 };
 
