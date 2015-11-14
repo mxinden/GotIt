8a425446566f36f38895dcf70edfa5bffdcfa976
Kristian Andersson <krisan13@uia.no>
2015-11-14 21:20:39 +0100

    Replace setTimout to setInterval when waiting for animation to finish.

--- tests/jasmine/client/integration/lecturePage/delete_question.js
+++ tests/jasmine/client/integration/lecturePage/delete_question.js
@@ -27,32 +27,24 @@ describe("Delete a question", function() {
   beforeAll(function(done) {
     Router.go('lecturePage', {lectureCode: lectureCode});
     Tracker.afterFlush(done);
-
   });
 
   beforeAll(waitForRouter);
-
-
 
   beforeAll(function(done) {
     waitForElement('.btn-delete-question', done);
   });
 
-  it("shows the question delete button", function() {
-    //jasmine.clock().tick( 500  );
-
-    expect('.btn-delete-question').toExist();
-  });
-
-
   beforeAll(function(done) {
     amountQuestionsBefore = Questions.find({lectureCode: lectureCode}).count();
-    jQuery.fx.off = true;
-
     $('.btn-delete-question').click();
-
-    done();
-
+    var interval = setInterval(function() {
+       question = $('.question-text:contains("'+ testQuestion + '")').length;
+       if(question === 0) {
+         clearInterval(interval);
+         done();
+       }
+     }, 100);
   });
 
   it("deletes the question in the questions collection", function() {
@@ -61,29 +53,11 @@ describe("Delete a question", function() {
   });
 
   it("removes the question template", function() {
-    setTimeout(function(){    expect('.btn-delete-question').not.toExist();
-}, 100);
-
-    //expect($('.question-text:contains("'+ testQuestion + '")')).not.toExist();
-
-
-
-  });
-
-  afterAll(function(done) {
-    jasmine.clock().uninstall();
-    jQuery.fx.off = false;
-
+      expect($('.question-text:contains("'+ testQuestion + '")')).not.toExist();
+      expect('.btn-delete-question').not.toExist();
   });
 
 
-});
 
-/*
-beforeAll(function(done) {
-amountQuestionsBefore = Questions.find({lectureCode: lectureCode}).count();
-amountVotesBefore = Votes.find({lectureCode: lectureCode}).count();
-//$('#create-question').click();
-waitForElement('.question', done);
+
 });
-*/
