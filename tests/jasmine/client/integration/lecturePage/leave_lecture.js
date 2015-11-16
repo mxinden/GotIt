f8fe957c4c850c3a34b7d1ba6b3f0568eec7981a
Gerrit <gerrit.seger@gmail.com>
2015-11-16 20:45:44 +0100

    Refactor leaveLecture spec

--- tests/jasmine/client/integration/lecturePage/leave_lecture.js
+++ tests/jasmine/client/integration/lecturePage/leave_lecture.js
@@ -1,54 +1,51 @@
-describe("Leaving the lecture", function() {
+"use strict";
 
-  var lectureCode = '00001';
+describe("Leaving the lecture", function() {
+  var amountVotesBefore;
+  var lectureCode = "00001";
 
   beforeAll(function(done) {
     Fixtures.clearDB(done);
   });
 
   beforeAll(function(done) {
-    Fixtures.createLecture({lectureCode: lectureCode}, function() {
-      done();
-    });
+    Fixtures.createLecture({lectureCode: lectureCode}, done);
   });
 
   beforeAll(function(done) {
-    Router.go('lecturePage', {lectureCode: lectureCode});
+    Router.go("lecturePage", {lectureCode: lectureCode});
     Tracker.afterFlush(done);
   });
 
   beforeAll(waitForRouter);
 
-  /** Create a new question and thereby a new vote */
   beforeAll(function(done) {
-    $('#question-text').val('I am going to leave!');
-    $('#btn-create-question').click();
-    waitForElement('.btn-unvote', function() {
+    $("#question-text").val("I am going to leave!");
+    $("#btn-create-question").click();
+    waitForElement(".btn-unvote", function() {
       amountVotesBefore = Votes.find({lectureCode: lectureCode}).count();
       done();
     });
   });
 
-  /** Leave the lecture */
   beforeAll(function(done) {
-    $('#back-button').click();
-    waitForElement('#landing-page', function() {
-      done();
-    });
+    $("#back-button").click();
+    waitForElement("#landing-page", done);
   });
 
-  it("Deletes all votes from user from lecture in the MongoDB", function() {
-    amountVotesAfter = Votes.find({lectureCode: lectureCode}).count();
+  it("deletes all votes from user from lecture in the MongoDB", function() {
+    var amountVotesAfter = Votes.find({lectureCode: lectureCode}).count();
+
     expect(amountVotesAfter).toBeLessThan(amountVotesBefore);
   });
 
-  it("Routes back to the landing page", function() {
-    expect(Router.current().route.getName()).toEqual('landingPage');
+  it("routes back to the landing page", function() {
+    expect(Router.current().route.getName()).toEqual("landingPage");
   });
 
-  it("Should delete the user from the members array of the lecture", function() {
-    amountMembers = App.Lectures.Collection.findOne({lectureCode: lectureCode}).members.length;
+  it("should delete the user from the members array of the lecture", function() {
+    var amountMembers = App.Lectures.Collection.findOne({lectureCode: lectureCode}).members.length;
+
     expect(amountMembers).toEqual(0);
   });
-
 });
