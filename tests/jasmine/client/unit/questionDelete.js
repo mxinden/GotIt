fdb62151831bcb716da01033b33ab5a7a56e4753
Gerrit <gerrit.seger@gmail.com>
2015-11-16 19:06:23 +0100

    Refactor question delete spec and add two specs

--- tests/jasmine/client/unit/questionDelete.js
+++ tests/jasmine/client/unit/questionDelete.js
@@ -1,42 +1,37 @@
 describe("questionDelete", function() {
+  beforeAll(function(done) {
+    Fixtures.clearDB(done);
+  });
+
   describe("when the user is not the lecture author", function() {
-    var callError, callResult, lectureCode, amountQuestionsAfter;
-    var amountQuestionsBefore = Questions.find().count();
+    var callError, callResult, amountQuestionsBefore;
     var lecture = {
-      author: 'Somebody else',
+      lectureCode: '00000',
+      author: 'Somebody else'
     };
     var question = {
-      lectureCode: '00000',
+      lectureCode: lecture.lectureCode,
     };
 
     beforeAll(function(done) {
-      Fixtures.clearDB(done);
-    });
-
-    beforeAll(function(done) {
-      Meteor.subscribe('lecture', question.lectureCode);
-      Meteor.subscribe('questions', question.lectureCode);
+      Meteor.subscribe('lecture', lecture.lectureCode);
+      Meteor.subscribe('questions', lecture.lectureCode);
       done();
     });
 
     beforeAll(function(done) {
-      Fixtures.createLecture(lecture, function(error, result) {
-        lectureCode = result;
-        done();
-      });
+      Fixtures.createLecture(lecture, done);
     });
 
     beforeAll(function(done) {
       Fixtures.createQuestion(question, function(error, result) {
-        question = {
-          lectureCode: '00000',
-          questionId: result
-        };
+        question.questionId = result;
         done();
       });
     });
 
     beforeAll(function(done) {
+      amountQuestionsBefore = Questions.find().count();
       Meteor.call('deleteQuestion', question, function(error, result) {
         callError = error;
         callResult = result;
@@ -49,46 +44,43 @@ describe("questionDelete", function() {
       expect(callError).not.toBe(undefined);
       expect(callError.error).toEqual('Not the lecture author');
     });
+
+    it("does not change the number of questions", function() {
+      var amountQuestionsAfter = Questions.find().count();
+
+      expect(amountQuestionsBefore).toEqual(amountQuestionsAfter);
+    });
   });
 
-  describe("when the user is  the lecture author", function() {
-    var callError, callResult, lectureCode, amountQuestionsBefore, amountQuestionsAfter;
+  describe("when the user is the lecture author", function() {
+    var callError, callResult, lectureCode, amountQuestionsBefore;
+    var lecture = {
+      lectureCode: '11111'
+    };
     var question = {
-      lectureCode: '00000',
+      _id: '12345678901234567',
+      lectureCode: lecture.lectureCode
     };
 
     beforeAll(function(done) {
-      Fixtures.clearDB(done);
-    });
-
-    beforeAll(function(done) {
-      Meteor.subscribe('lecture', question.lectureCode);
-      Meteor.subscribe('questions', question.lectureCode);
+      Meteor.subscribe('lecture', lecture.lectureCode);
+      Meteor.subscribe('questions', lecture.lectureCode);
       done();
     });
 
     beforeAll(function(done) {
       var interval = setInterval(function() {
         if (Meteor.userId() !== null) {
-          Fixtures.createLecture({author: Meteor.userId()}, function(error, result) {
-            lectureCode = result;
-            done();
-          });
+          lecture.author = Meteor.userId();
+          Fixtures.createLecture(lecture, done);
         }
       }, 100);
     });
 
     beforeAll(function(done) {
-      amountQuestionsBefore = Questions.find().count();
-      done();
-    });
-
-    beforeAll(function(done) {
       Fixtures.createQuestion(question, function(error, result) {
-        question = {
-          lectureCode: '00000',
-          questionId: result
-        };
+        amountQuestionsBefore = Questions.find().count();
+        question.questionId = result;
         done();
       });
     });
@@ -97,14 +89,18 @@ describe("questionDelete", function() {
       Meteor.call('deleteQuestion', question, function(error, result) {
         callError = error;
         callResult = result;
-        amountQuestionsAfter = Questions.find().count();
         done();
       });
     });
 
     it('does not throw an error', function() {
       expect(callError).toBe(undefined);
-      expect(amountQuestionsBefore).toEqual(amountQuestionsAfter);
+    });
+
+    it("decreases the amount of questions", function() {
+      var amountQuestionsAfter = Questions.find().count();
+
+      expect(amountQuestionsBefore).toEqual(amountQuestionsAfter + 1);
     });
   });
 });
