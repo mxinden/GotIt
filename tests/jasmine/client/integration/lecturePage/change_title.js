94b7937ba6e47ea67b0f1531aad40bc7348fc6ea
Gerrit <gerrit.seger@gmail.com>
2015-11-16 21:12:37 +0100

    Refactor changeTitle spec

--- tests/jasmine/client/integration/lecturePage/change_title.js
+++ tests/jasmine/client/integration/lecturePage/change_title.js
@@ -1,36 +1,38 @@
-describe('Change class room title', function() {
+"use strict";
+
+describe("Change class room title", function() {
   beforeAll(function(done) {
     Fixtures.clearDB(done);
   });
 
-  describe('for non authors', function() {
-    var lectureCode = 'AAAAA';
+  describe("for non authors", function() {
+    var lectureCode = "AAAAA";
 
     beforeAll(function(done) {
       Fixtures.createLecture({lectureCode: lectureCode}, done);
     });
 
     beforeAll(function(done) {
-      Router.go('lecturePage', {lectureCode: lectureCode});
+      Router.go("lecturePage", {lectureCode: lectureCode});
       Tracker.afterFlush(done);
     });
 
     beforeAll(waitForRouter);
 
     beforeAll(function(done) {
-      waitForElement('#title', function() {
-        $('#title').trigger('click');
+      waitForElement("#title", function() {
+        $("#title").trigger("click");
         done();
       });
     });
 
-    it('should not be changeable', function() {
-      expect($('#title-input')).not.toExist();
+    it("should not be changeable", function() {
+      expect($("#title-input")).not.toExist();
     });
   });
 
-  describe('for authors', function() {
-    var lectureCode = 'BBBBB';
+  describe("for authors", function() {
+    var lectureCode = "BBBBB";
 
     beforeAll(function(done) {
       Fixtures.createLecture({
@@ -40,45 +42,45 @@ describe('Change class room title', function() {
     });
 
     beforeAll(function(done) {
-      Router.go('lecturePage', {lectureCode: lectureCode});
+      Router.go("lecturePage", {lectureCode: lectureCode});
       Tracker.afterFlush(done);
     });
 
     beforeAll(waitForRouter);
 
     beforeAll(function(done) {
-      waitForElement('#lecture-page', done);
+      waitForElement("#lecture-page", done);
     });
 
     beforeAll(function(done) {
-      $('#title').trigger('click');
+      $("#title").trigger("click");
       done();
     });
 
-    it('should be changeable', function(done) {
-      waitForElement('#title-input', function() {
-        expect($('#title')).not.toExist();
-        expect($('#title-input')).toExist();
+    it("should be changeable", function(done) {
+      waitForElement("#title-input", function() {
+        expect($("#title")).not.toExist();
+        expect($("#title-input")).toExist();
         done();
       });
     });
 
-    describe('title update', function() {
+    describe("title update", function() {
       beforeAll(function() {
-        $('#title-input').val('I like trains');
-        $('#title-input').blur();
+        $("#title-input").val("I like trains");
+        $("#title-input").blur();
       });
 
-      it('updates the title in the database', function() {
+      it("updates the title in the database", function() {
         var interval = setInterval(function() {
-          var lecture = App.Lectures.Collection.findOne({lectureCode: lectureCode, title: 'I like trains'});
+          var lecture = App.Lectures.Collection.findOne({lectureCode: lectureCode, title: "I like trains"});
           if (lecture) {
+            // Timeout will indicate a failing spec here
             expect(true).toBe(true);
             clearInterval(interval);
           }
         }, 100);
       });
-
     });
   });
 });
