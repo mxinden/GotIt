792e7a838c92981e1059a2d457c4fae11d0b87fa
minden <IndenML@gmail.com>
2015-11-16 17:06:04 +0100

    Refactor lib/applications.js

--- client/lib/application.js
+++ client/lib/application.js
@@ -1,10 +1,9 @@
 getNumberOfMembersInLecture = function(lectureCode) {
-  lecture = Lectures.findOne({lectureCode: lectureCode}, {members: true, author: true});
-  author = Lectures.findOne({lectureCode: lectureCode, members: lecture.author}, {author: true});
+  var lecture = Lectures.findOne({lectureCode: lectureCode}, {members: true, author: true});
+  var author = Lectures.findOne({lectureCode: lectureCode, members: lecture.author}, {author: true});
 
   if (author !== undefined) {
     return lecture.members.length - 1;
-  } else {
-    return lecture.members.length;
   }
+  return lecture.members.length;
 };
