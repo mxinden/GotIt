e04ebc7e2cb8a1728ed87cf4d1675847607d2502
Gerrit <gerrit.seger@gmail.com>
2015-11-16 19:20:09 +0100

    Refactor insertVote spec

--- tests/jasmine/client/unit/insertVote.js
+++ tests/jasmine/client/unit/insertVote.js
@@ -1,15 +1,12 @@
-describe('insertVote', function() {
+"use strict";
 
-  describe('with a wrong questionId', function() {
-
-    var callError;
-    var callResult;
+describe("insertVote", function() {
+  describe("with a wrong questionId", function() {
+    var callError, callResult;
     var amountVotesBefore = Votes.find().count();
-    var amountVotesAfter = Votes.find().count();
-
     var vote = {
-      questionId: 'NotAnExistingQuestion',
-      lectureCode: '00000'
+      questionId: "NotAnExistingQuestion",
+      lectureCode: "00000"
     };
 
     beforeAll(function(done) {
@@ -17,29 +14,28 @@ describe('insertVote', function() {
     });
 
     beforeAll(function(done) {
-      Meteor.subscribe('votes', vote.lectureCode);
+      Meteor.subscribe("votes", vote.lectureCode);
       done();
     });
 
     beforeAll(function(done) {
-      Meteor.call('insertVote', vote, function(error, result) {
+      Meteor.call("insertVote", vote, function(error, result) {
         callError = error;
         callResult = result;
         done();
       });
     });
 
-    it('throws an error', function() {
+    it("throws an error", function() {
       expect(callResult).toBe(undefined);
       expect(callError).not.toBe(undefined);
-      expect(callError.error).toEqual('Question for this vote does not exist');
+      expect(callError.error).toEqual("Question for this vote does not exist");
     });
 
     it("does not create a new vote in the mongodb", function() {
       var amountVotesAfter = Votes.find().count();
+
       expect(amountVotesBefore).toEqual(amountVotesAfter);
     });
-
   });
-
 });
