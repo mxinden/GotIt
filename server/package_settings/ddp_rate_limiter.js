d009135f1e21003766e5ff7ea0a326e321e2ab39
Gerrit <gerrit.seger@gmail.com>
2015-11-29 16:01:58 +0100

    Fix process environment check

--- server/package_settings/ddp_rate_limiter.js
+++ server/package_settings/ddp_rate_limiter.js
@@ -2,13 +2,14 @@
 
 var setRateLimitToAllMeteorMethods;
 
-// Any Meteor method can only be called 5 times each second
-if (!process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
   setRateLimitToAllMeteorMethods = {
     type: 'method',
     name: function() {
       return true;
     }
   };
+
+  // Any Meteor method can only be called 5 times each second
   DDPRateLimiter.addRule(setRateLimitToAllMeteorMethods, 10, 1000);
 }
