1a5ee5dd45a200aca4c7ddaa29f89782381603ea
minden <IndenML@gmail.com>
2015-11-29 14:12:54 +0100

    Add minor eslint changes

--- server/package_settings/ddp_rate_limiter.js
+++ server/package_settings/ddp_rate_limiter.js
@@ -1,10 +1,14 @@
+"use strict";
+
+var setRateLimitToAllMeteorMethods;
+
 // Any Meteor method can only be called 5 times each second
 if (!process.env.NODE_ENV === 'development') {
-  var setRateLimitToAllMeteorMethods= {
+  setRateLimitToAllMeteorMethods = {
     type: 'method',
     name: function() {
-      return true
+      return true;
     }
-  }
+  };
   DDPRateLimiter.addRule(setRateLimitToAllMeteorMethods, 10, 1000);
 }
