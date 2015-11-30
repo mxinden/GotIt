c325c012c9f8ada229af12eb58df51fece345b87
Gerrit <gerrit.seger@gmail.com>
2015-11-30 17:04:38 +0100

    Final updates to make factories and traits work

--- packages/factorywoman/factorywoman.js
+++ packages/factorywoman/factorywoman.js
@@ -4,50 +4,60 @@ if (Meteor.isClient) {
   var Factory, FactoryClosure;
 
   FactoryWoman = {
-    self: this,
-
     _factories: {},
     begin: function(func, callback, func_count) {
+      console.log('Beginning with new closure '); 
       new FactoryClosure(func, callback, func_count);
     },
     create: function(name, changes, traits) {
+      // this refering to a FactoryClosure object
+      var self = this;
       var result, factory;
 
+      console.log('Calling factory ' + name);
       factory = FactoryWoman._factories[name];
       if (factory === undefined) {
         console.error('Factory ' + name + ' is not defined.');
         return;
       }
 
-      result = factory._attr;
-      result._id = null;
+      result = _.clone(factory._attr);
       _.extend(result, changes);
 
-      Meteor.call('factoryWomanInsert', factory._collection, factory._attr, function(error, result) {
+      console.log('Inserting factory ' + name);
+      console.log('Collection: ' + factory._collection);
+      console.log('Attr: ' + factory._attr);
+      Meteor.call('factoryWomanInsert', factory._collection, result, function(error, insertResult) {
         if (error) {
           console.error('Error creating factory ' + name + '.');
-          this._counter++;
+          console.error(error);
+          self._counter++;
           return;
         }
 
-        result._id = result;
+        result._id = insertResult;
 
+        console.log('Done inserting factory ' + name);
         if (traits === undefined)
-          return;
+          traits = [];
         else if (typeof traits === 'string')
           traits = [traits];
 
         _.each(traits, function(trait) {
-          // this refering to a FactoryClosure object
-          this._stack.push({
+          var traitReturn;
+
+          console.log('Calling trait ' + trait);
+          traitReturn = factory._traits[trait].call(self, result);
+          _.extend(result, traitReturn);
+          self._stack.push({
             collection: factory._collection,
-            attr: result
+            attr: traitReturn,
+            result: result
           });
-          _.extend(result, trait.call(this, result));
         });
 
-        // this refering to a FactoryClosure object
-        this._counter++;
+        self._counter++;
+        console.log('===== INCREASED FUNCTION COUNTER TO ' + self._counter + ' =====');
       });
 
       return result;
@@ -75,6 +85,7 @@ if (Meteor.isClient) {
     this._counter = 0;
     this._trait_counter = 0;
     this._stack = [];
+    console.log('Starting closure worker');
     this._interval = setInterval(function() {
       self.closureWorker();
     }, 50);
@@ -83,31 +94,48 @@ if (Meteor.isClient) {
   };
 
   FactoryClosure.prototype.create = function(name, changes, traits) {
-    FactoryWoman.create.call(this, name, changes, traits);
+    return FactoryWoman.create.call(this, name, changes, traits);
   };
 
   FactoryClosure.prototype.closureWorker = function() {
-    if (this._counter === this._func_count) {
+    var self = this;
+    console.log(this._counter);
+    console.log(this._func_count);
+    console.log(this._trait_counter);
+    console.log(this._stack);
+    if (this._counter >= this._func_count) {
+      console.log('WOOOT');
       // we got all functions running, time to check the stack
       if (this._stack.length > 0) {
+        console.log('Found object on stack');
         var obj = this._stack.pop();
 
-        if (obj.attr._id === undefined) {
+        if (obj.result._id === undefined) {
+          console.log('_id still undefined, aborting');
           this._stack.push(obj);
           return;
         }
 
+        console.log('_id defined, updating');
         this._trait_counter++;
-        Meteor.call('factoryWomanUpdate', obj.attr._id, obj.collection, obj.attr, function(error) {
+        Meteor.call('factoryWomanUpdate', obj.result._id, obj.collection, obj.attr, function(error) {
           if (error)
             console.error('Error updating trait');
 
-          this._trait_counter--;
+          console.log('Updating with: ********* ');
+          console.log(obj.result._id);
+          console.log(obj.attr);
+          _.extend(obj.result, obj.attr);
+          console.log('Done updating trait');
+          self._trait_counter--;
         });
       }
     }
 
-    if ((this._counter === this._func_count) && (this._trait_counter === 0) && (this._stack === [])) {
+    if ((this._counter >= this._func_count)
+        && (this._trait_counter === 0)
+        && (this._stack.length === 0)) {
+      console.log('Closure finished');
       clearInterval(this._interval);
       this._callback();
     };
